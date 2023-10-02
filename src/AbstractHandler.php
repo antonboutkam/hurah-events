a263ce2ece3550511958280ed37209cfbe7647a3
Anton Boutkam <anton@nui-boutkam.nl>
2023-10-02 16:58:38 +0200

    - Extracted Context to an interface to be able to provide statically typed arguments. - Fixed some unit tests

--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -113,7 +113,7 @@ abstract class AbstractHandler implements HandlerInterface
 
         }
     }
-    abstract protected function handleTask(Context $oContext):int;
+    abstract protected function handleTask(ContextInterface $oContext):int;
     protected function maxAttempts():int
     {
         return 0;
